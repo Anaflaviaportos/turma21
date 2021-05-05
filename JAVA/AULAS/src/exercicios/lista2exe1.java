0830bee1ad33fc48c48314fb9bd27230c3bd2ff4
Anaflaviaportos <anaflaviaportoneves@gmail.com>
2021-05-05 13:46:55 -0300

    arquivos aualizado lista2

---
+++ JAVA/AULAS/src/exercicios/lista2exe1.java
@@ -0,0 +1,46 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class lista2exe1 {
+	
+	public static void main(String[] args) {
+		
+		//Faça um programa que receba três inteiros e diga qual deles é o maior.//
+		
+		Scanner leia = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+		
+		System.out.println("Digite o primeiro numero inteiro: \n");
+		num1 = leia.nextInt();
+		System.out.println("Digite o segungo numero inteiro: \n");
+		num2 = leia.nextInt();
+		System.out.println("Digite o terceiro numero inteiro: \n");
+		num3 = leia.nextInt();
+		
+		
+		System.out.printf("Primeiro numero: %d \nSegundo numero: %d \nTerceiro numero: %d \n",num1,num2,num3);
+		
+		if (num1>num2 && num1>num3)
+		{
+		System.out.printf("\nO primeiro numero é maior!");
+		}
+		
+		else if (num2>num1 && num2>num3)
+		{
+		System.out.printf("\nO segundo numero é maior!");
+		}
+		
+		else
+		{ 
+		System.out.printf("\nO terceiro numero é maior!");
+		}
+		
+	}
+
+}
---
+++ JAVA/AULAS/src/exercicios/lista2exe3.java
@@ -0,0 +1,42 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class lista2exe3 {
+
+	public static void main(String[] args) {
+		//Faça um programa que receba a idade de uma pessoa e mostre na saída em qual categoria ela se encontra:
+		//10-14 infantil 15-17 juvenil 18-25 adulto
+
+		Scanner leia = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		int idade = 0;
+		
+		System.out.println("Digite a sua idade:");
+		idade = leia.nextInt();
+		
+		if (idade>=10 && idade<=14)
+		{
+			System.out.printf("Você tem %d anos e faz parte da categoria infantil",idade);
+		}
+		
+		else if (idade>=15 && idade<=17)
+		{
+			System.out.printf("Você tem %d anos e faz parte da categoria juvenil",idade);
+		}
+		
+		else if (idade>=18 && idade<=25)
+		{
+			System.out.printf("Você tem %d anos e faz parte da categoria adulto",idade);
+		}
+		
+		else 
+		{
+			System.out.printf("Categoria de idade não classificada!");
+		}
+		
+	}
+
+}
