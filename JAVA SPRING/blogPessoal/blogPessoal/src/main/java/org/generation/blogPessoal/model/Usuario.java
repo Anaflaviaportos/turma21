fe612dd3e994fda0b061fa181e884b6fd6a9be90
Anaflaviaportos <anaflaviaportoneves@gmail.com>
2021-06-08 18:24:25 -0300

    Security back-and blog pessoal 2

--- JAVA SPRING/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ JAVA SPRING/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name = "tb_usuarios")
 public class Usuario {
 
 	@Id
